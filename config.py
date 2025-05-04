d337898f1ca1ea6ffd19739743b6edf50a36d23b
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-05-04 12:04:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 import os
 
-# Environment variables
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-PIXABAY_API_KEY = os.getenv('PIXABAY_API_KEY')
-PEXELS_API_KEY = os.getenv('PEXELS_API_KEY')
-DEFAULT_RESULT_COUNT = int(os.getenv('DEFAULT_RESULT_COUNT', 10))  # Default value if not set
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+PEXELS_API_KEY = os.getenv("PEXELS_API_KEY")
+PIXABAY_API_KEY = os.getenv("PIXABAY_API_KEY")
